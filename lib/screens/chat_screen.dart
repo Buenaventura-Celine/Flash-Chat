29fe034027b6c4f708556d7e2885fd19c8178261
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-26 14:42:27 +0800

    Turning Streams into widgets using stream builder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,12 +33,12 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void messagesStream() async {
-    final messages = await _firestore.collection('messages').get();
-    for (var message in messages.docs) {
-      print(message.data());
-    }
-  };
+  // void messagesStream() async {
+  //   //   final messages = await _firestore.collection('messages').get();
+  //   //   for (var message in messages.docs) {
+  //   //     print(message.data());
+  //   //   }
+  //   // };
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +51,7 @@ class _ChatScreenState extends State<ChatScreen> {
               onPressed: () {
                 // _auth.signOut();
                 // Navigator.pop(context);
-                messagesStream();
+                // messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
@@ -62,6 +62,31 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+                final messages = snapshot.data.docs;
+                List<Text> messageWidgets = [];
+                for (var message in messages) {
+                  final messageText = message.get('text');
+                  final messageSender = message.get('sender');
+
+                  final messageWidget =
+                      Text('$messageText from $messageSender');
+                  messageWidgets.add(messageWidget);
+                }
+                return Column(
+                  children: messageWidgets,
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
